0ff1960fe6c6b167bf4650958ec54573cd869c9e
AlexeyDrobkov <alexey.drobkov@railsmuffin.com>
2023-10-01 12:29:32 +0530

    post ownership

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_01_053000) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_01_062440) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -45,6 +45,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_01_053000) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.binary "image"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -61,4 +63,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_01_053000) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "posts", "users"
 end
