95b12d115f2bbe1b48527c1c23f27095875a2d9b
AlexeyDrobkov <alexey.drobkov@railsmuffin.com>
2023-10-04 08:18:21 +0530

    comments done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
-    has_one_attached :image
     belongs_to :user
+    has_one_attached :image
+    has_many :comments, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
 
-  resources :posts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # resources :posts
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+  end
 
   resources :users do
     member do
